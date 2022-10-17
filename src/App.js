c83f2899e426464b914fbe6ac4d5fdf77497899f
ReA974 <39021553+ReA974@users.noreply.github.com>
2022-10-17 14:13:02 +0200

    ajout ville/poi

--- src/App.js
+++ src/App.js
@@ -1,60 +1,157 @@
 import { useQuery, gql } from '@apollo/client';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import React, { useEffect, useState } from "react";
 
-const GET_LOCATIONS = gql`	
-{
-  poi(
-    filters: [{
-      isLocatedAt: {
-        schema_address: {
-          schema_addressLocality: {
-            _eq: "La Rochelle"
+export default function App() {
+  const [ville, setVille] = useState("");
+  const [poi, setPoi] = useState("");
+  
+  var GET_LOCATIONS = gql`	
+  {
+    poi(
+      filters: [{
+        isLocatedAt: {
+          schema_address: {
+            schema_addressLocality: {
+              _eq: "La Rochelle"
+            }
+          }
+        }
+      }
+      ]) {
+      total results {
+          rdfs_label{
+            value 
+          }
+          isLocatedAt{
+          schema_geo{
+            schema_latitude
+            schema_longitude
           }
         }
       }
     }
-    ]) {
-    total results {
-        rdfs_label{
-          value 
+  }
+  `;
+  
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    if(ville !== "" && poi !== ""){
+    GET_LOCATIONS = gql`	
+     {
+      poi(
+        filters: [{
+          rdf_type: {
+            _eq: "https://www.datatourisme.fr/ontology/core#${poi}"
+          }
+        }, {
+          isLocatedAt: {
+            schema_address: {
+              schema_addressLocality: {
+                _eq: "${ville}"
+              }
+            }
+          }
         }
-        isLocatedAt{
-        schema_geo{
-          schema_latitude
-          schema_longitude
+        ]) {
+          total results {
+            rdfs_label{
+              value 
+            }
+            isLocatedAt{
+            schema_geo{
+              schema_latitude
+              schema_longitude
+            }
+          }
         }
       }
     }
+  `;
+    }
+    if(ville !== "" && poi === ""){
+      console.log(ville);
+      GET_LOCATIONS = gql`	
+      {
+       poi(
+         filters: [{
+           isLocatedAt: {
+             schema_address: {
+               schema_addressLocality: {
+                 _eq: "${ville}"
+               }
+             }
+           }
+         }
+         ]) {
+           total results {
+             rdfs_label{
+               value 
+             }
+             isLocatedAt{
+             schema_geo{
+               schema_latitude
+               schema_longitude
+             }
+           }
+         }
+       }
+     }
+   `;
+   console.log(GET_LOCATIONS);
+    }
+    if(poi !== "" && ville === ""){
+      GET_LOCATIONS = gql`	
+      {
+       poi(
+         filters: [{
+           rdf_type: {
+             _eq: "https://www.datatourisme.fr/ontology/core#${poi}"
+           }
+         }
+         ]) {
+           total results {
+             rdfs_label{
+               value 
+             }
+             isLocatedAt{
+             schema_geo{
+               schema_latitude
+               schema_longitude
+             }
+           }
+         }
+       }
+     }
+   `;
+    }
   }
-}
-`;
 
-function DisplayLocations() {
-  const { loading, error, data } = useQuery(GET_LOCATIONS);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-  const positionFirstPoi = [data.poi.results[0].isLocatedAt[0].schema_geo[0].schema_latitude[0],data.poi.results[0].isLocatedAt[0].schema_geo[0].schema_longitude[0]];
-  return (
+  function DisplayLocations() {
+    const { loading, error, data } = useQuery(GET_LOCATIONS);
   
-  <MapContainer center={positionFirstPoi} zoom={13} scrollWheelZoom={true} style={{ height: 800, width: 1200 }}>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    {data.poi.results.map(({ isLocatedAt,rdfs_label }) => (
-     <Marker position={[isLocatedAt[0].schema_geo[0].schema_latitude[0],isLocatedAt[0].schema_geo[0].schema_longitude[0]]}>
-      <Popup>
-       {rdfs_label[0].value}
-      </Popup>
-    </Marker>
-    ))}
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+    const positionFirstPoi = [data.poi.results[0].isLocatedAt[0].schema_geo[0].schema_latitude[0],data.poi.results[0].isLocatedAt[0].schema_geo[0].schema_longitude[0]];
+    return (
+    
+    <MapContainer center={positionFirstPoi} zoom={13} scrollWheelZoom={true} style={{ height: 800, width: 1200 }}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {data.poi.results.map(({ isLocatedAt,rdfs_label }) => (
+       <Marker position={[isLocatedAt[0].schema_geo[0].schema_latitude[0],isLocatedAt[0].schema_geo[0].schema_longitude[0]]}>
+        <Popup>
+         {rdfs_label[0].value}
+        </Popup>
+      </Marker>
+      ))}
+  
+    </MapContainer>
+    );
+  }
 
-  </MapContainer>
-  );
-}
-
-export default function App() {
   return (
     <div>
       <h2>
@@ -63,6 +160,25 @@ export default function App() {
           🗺️
         </span>
       </h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Ville:
+          <input
+            type="text"
+            value={ville}
+            onChange={e => setVille(e.target.value)}
+          />
+        </label>
+        <label>
+          Poi:
+          <input
+            type="text"
+            value={poi}
+            onChange={e => setPoi(e.target.value)}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
       <br />
       <div>
         <DisplayLocations />
